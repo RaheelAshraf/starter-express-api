3943b2b77d613c4fa2b6a00efe79b4e6dd63df49
labkoat <labkoat@gmail.com>
2023-09-27 02:51:13 +0500

    testing

--- index.js
+++ index.js
@@ -13,40 +13,45 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.get('/', (req, res) => {
+    res.send(`Hello world`)
+})
+
 // Define a function to query Pinecone and get a response
 async function queryPineconeAndReturnResponse(client, indexName, question) {
-  try {
-    const response = await queryPineconeVectorStoreAndQueryLLM(client, indexName, question);
+    try {
+        const response = await queryPineconeVectorStoreAndQueryLLM(client, indexName, question);
 
-    return response;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
 }
 
 // POST route to generate a response
 app.post('/generate', async (req, res) => {
-  const question = req.body.text;
-  
-  const indexName = "uindex";
-  const client = new PineconeClient();
-  await client.init({
-    apiKey: process.env.PINECONE_API_KEY,
-    environment: process.env.PINECONE_ENVIRONMENT,
-  });
-  try {
-    const response = await queryPineconeAndReturnResponse(client, indexName, question);
-    console.log(`ResponseType: ${response}`)
-    res.send(response); // Send the response back to the client (HTML page)
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('An error occurred.');
-  }
+    const question = req.body.text;
+
+    const indexName = "uindex";
+    const client = new PineconeClient();
+    await client.init({
+        apiKey: process.env.PINECONE_API_KEY,
+        environment: process.env.PINECONE_ENVIRONMENT,
+    });
+    try {
+        const response = await queryPineconeAndReturnResponse(client, indexName, question);
+        console.log(`ResponseType: ${response}`)
+        res.send(response); // Send the response back to the client (HTML page)
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred.');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
 
