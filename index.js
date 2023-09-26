c4be7867e9cb9cf7862fb0a800978639a771fb7f
labkoat <labkoat@gmail.com>
2023-09-27 02:17:13 +0500

    testing

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo0000!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { PineconeClient } = require('@pinecone-database/pinecone');
+const dotenv = require('dotenv');
+const { queryPineconeVectorStoreAndQueryLLM } = require('./query.js');
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Define a function to query Pinecone and get a response
+async function queryPineconeAndReturnResponse(client, indexName, question) {
+    try {
+        const response = await queryPineconeVectorStoreAndQueryLLM(client, indexName, question);
+
+        return response;
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+// POST route to generate a response
+app.post('/generate', async (req, res) => {
+    const question = req.body.text;
+
+    const indexName = "uindex";
+    const client = new PineconeClient();
+    await client.init({
+        apiKey: process.env.PINECONE_API_KEY,
+        environment: process.env.PINECONE_ENVIRONMENT,
+    });
+    try {
+        const response = await queryPineconeAndReturnResponse(client, indexName, question);
+        console.log(`ResponseType: ${response}`)
+        res.send(response); // Send the response back to the client (HTML page)
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+//OPENAI_API_KEY=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
